2246e2b9fbffcdc0f715030860c1efcdbe65cabd
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2023-05-24 16:23:39 +0530

    added promises questions

---
+++ Promises/promises.js
@@ -0,0 +1,8 @@
+// what is promises in javascript?
+// ans - to handle asynchronus operation we use promises before promises we use callbacks to handle async operations
+// but using callback again and again make callback hells and this is a hectic approach. to overcome this es6 gives promises
+// with the help of that we can handle async operations in a structured way. 
+// as the name tells promises promise us to handle async operations in three states
+// if promise is pending that means we have to wait till it fulfilled or rejected.
+// if promise is fulfilled that means operation runs successfully.
+// if promise is rejected that means operation gives us error
